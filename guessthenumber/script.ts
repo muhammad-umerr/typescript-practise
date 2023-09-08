40370d551b08cda1b3dddcf463a8cb8be4cd45b0
muhammad-umerr <orbitplays442@gmail.com>
2023-09-08 08:58:25 -0700

    gtn

---
+++ guessthenumber/script.ts
@@ -0,0 +1,12 @@
+enum eSections {
+    SECTION_START,
+    SECTION_GUESS,
+    SECTION_RESULT
+}
+
+const Sections = [
+    document.getElementsByClassName("mainSection"),
+    document.getElementsByClassName("mainSection1"),
+    document.getElementsByClassName("mainSection2")
+]
+
